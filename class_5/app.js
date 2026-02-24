02abaa0dc0442a25d1145f23f67e01e4edc8b015
Amirshah98760 <amirshah98760@gmail.com>
2026-02-24 22:16:14 +0500

    Add the post api and setup the multur to read the image

--- class_5/app.js
+++ class_5/app.js
@@ -1,7 +1,40 @@
 const express = require('express');
-
+const postModel = require('./models/post.model');
+const multer = require('multer');
 const app = express();
 
+app.use(express.json());
+const upload = multer({storage: multer.memoryStorage()});
+
+app.get('/', (req , res)=>{
+    res.send("Welcome to the home page");
+})
+
+
+
+app.post('/create-post',upload.single("image"), async (req, res )=>{
+  console.log(req.body);
+    console.log(req.file);
+
+//    try {
+//     const {image, caption} = req.body;
+
+//     const post = new postModel({
+//         image,
+//         caption
+//     })
+//     post.save();
+//     res.status(201).json({
+//         message:"Post created successfully",
+//         post
+//     })
+    
+//    } catch (error) {
+//     res.status(500).json({
+//         error:error.message
+//     })
+//    }
+})
 
 
 
--- class_5/server.js
+++ class_5/server.js
@@ -1,10 +1,11 @@
 const app = require('./app');
 const connectDB = require('./config/db');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 connectDB();  
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 
 app.listen(PORT, ()=>{
