55072c9c7e79d85d74ef3e497ea6d020ddbd26d2
Amirshah98760 <amirshah98760@gmail.com>
2026-02-24 21:15:05 +0500

    Create a basic Express server and connected to database

---
+++ class_5/config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+
+const connectDB = async () =>{
+    try {
+       await mongoose.connect(process.env.MONGODB_URI);
+       console.log("Mongodb is connected");
+
+    } catch (error) {
+        console.log("Error connecting to MongoDB:", error.message);
+    }
+}
+
+module.exports = connectDB;
