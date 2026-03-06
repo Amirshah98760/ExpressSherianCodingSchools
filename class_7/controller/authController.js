564fe4c4a7cc300e6dd9dcc9f7243998fad7e7fc
Amirshah98760 <amirshah98760@gmail.com>
2026-03-06 14:45:54 +0500

    Add the login api

--- class_7/controller/authController.js
+++ class_7/controller/authController.js
@@ -50,7 +50,52 @@ const registerUser = async (req, res) => {
     }
 };
 
+const loginUser = async (req, res) => {
+    // Implement login logic here
+
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({
+            message: "All fields are required"
+        });
+    }
+
+        try {
+            const user = await userModel.findOne({ email });
+            if (!user) {
+                return res.status(404).json({
+                    message: "User not found"
+                });
+            }
+            if (user.password !== password) {
+                return res.status(401).json({
+                    message: "Invalid credentials"
+                });
+            }
+
+            const token = jwt.sign(
+                { id: user._id },
+                process.env.JWT_SECRET,
+                { expiresIn: "1d" }
+            );
+            user.password = undefined;
+
+            res.cookie("token", token);
+            res.status(200).json({
+                message: "User logged in successfully",
+                user,
+            });
+        } catch (error) {
+            res.status(500).json({
+                message: error.message
+            });
+        }
+
+}
+
 
 module.exports = {
     registerUser,
+    loginUser
 };
